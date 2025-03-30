78fdec14f9d5af8162741534e0787f6d70da283e
HoangLinh2k6 <do16092006@gmail.com>
2025-03-30 21:25:16 +0700

    refactor graphics module

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@
 int main( int argc , char* argv[] ){
     window win;
     win.init();
+    bool quit = false;
+    SDL_Event event;
+    while ( !quit ){
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) quit = true;
+        }
+    }
     win.quit();
     return 0;
 }
